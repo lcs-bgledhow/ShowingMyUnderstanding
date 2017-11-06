de479dae7e29229b7a06b8273bfff92e1fe1515f
Ben Gledhow <bgledhow@lcs-mba3210.lcs.on.ca>
2017-11-06 15:28:34 -0500

    Progress on Sequence practice

--- ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
+++ ShowingMyUnderstanding.playground/Pages/Sequence - Create an Example.xcplaygroundpage/Contents.swift
@@ -21,12 +21,49 @@ import Cocoa
 import PlaygroundSupport
 
 // Create canvas
-let canvas = Canvas(width: 300, height: 300)
+let canvas = Canvas(width: 350, height: 350)
 
-// Replace this comment and add your code below...
+canvas.drawShapesWithBorders = false
+
+// black background
+canvas.drawRectangle(bottomLeftX: 0, bottomLeftY: 0, width: 350, height: 350)
+// green circle
+
+canvas.fillColor = Color(hue: 120, saturation: 90, brightness: 100, alpha: 90)
+
+canvas.drawEllipse(centreX: 100, centreY: 185, width: 100, height: 100)
+
+canvas.lineColor = Color(hue: 120, saturation: 90, brightness: 100, alpha: 90)
+
+
+
+// yellow circle
+
+canvas.fillColor = Color(hue: 60, saturation: 100, brightness: 100, alpha: 80)
+
+canvas.drawEllipse(centreX: 150, centreY: 185, width: 100, height: 100)
+
+// red cicrle
+
+canvas.fillColor = Color(hue: 0, saturation: 100, brightness: 100, alpha: 150)
+
+canvas.drawEllipse(centreX: 200, centreY: 185, width: 100, height: 100)
+
+// blue circle
+
+canvas.fillColor = Color(hue: 240, saturation: 100, brightness: 95, alpha: 80)
+
+canvas.drawEllipse(centreX: 250, centreY: 185, width: 100, height: 100)
+
+
+
+
+
 
 /*:
- ## Template code
- The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+## Template code
+
+
+The code below is necessary to see results in the Assistant Editor at right.Please do not remove.
  */
 PlaygroundPage.current.liveView = canvas.imageView
